281fe43cc7897723bf188313373a2ac688d0f643
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-12-15 12:15:32 -0700

    Uploading Template

---
+++ pex2sorts.c
@@ -0,0 +1,23 @@
+/** pex2sorts.c
+ * ===========================================================
+ * Name:
+ * Section:
+ * Project: PEX2
+ * Purpose: Sorting algoritms
+ * Documentation:
+ * =========================================================== */
+void selectionSort(int array[], int N) {
+
+}
+
+void insertionSort(int array[], int N) {
+
+}
+
+void bubbleSort(int array[], int N) {
+
+}
+
+void mergeSort(int array[], int N) {
+
+}
---
+++ pex2sorts.h
@@ -0,0 +1,13 @@
+/** pex2sorts.h
+ * ===========================================================
+ * Name:
+ * Section:
+ * Project: PEX2
+ * Purpose: Sorting algoritms
+ * Documentation:
+ * =========================================================== */
+
+void selectionSort(int array[], int N);
+void insertionSort(int array[], int N); 
+void bubbleSort(int array[], int N);
+void mergeSort(int array[], int N);
